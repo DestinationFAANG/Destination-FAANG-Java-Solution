24869952b0a484ba93e98c429ea025ef4a2f72d9
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-05-26 16:03:04 -0400

    Create 424 Longest Repeating Character Replacement.java

---
+++ 424 Longest Repeating Character Replacement/424 Longest Repeating Character Replacement.java
@@ -0,0 +1,26 @@
+//LeetCode Link: https://leetcode.com/problems/longest-repeating-character-replacement/
+// Youtube Link: 
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        
+        int[] occurance = new int[26];
+        int left = 0;
+        int ans = 0;
+        int maxOccurance = 0;
+        
+        for(int right = 0; right < s.length(); right++){
+            maxOccurance = Math.max(maxOccurance, ++occurance[s.charAt(right) - 'A']);
+            if(right - left + 1 - maxOccurance > k){
+                
+                occurance[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, right-left + 1);
+        }
+        
+        return ans;
+        
+        
+    }
+}
