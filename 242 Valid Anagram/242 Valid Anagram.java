e7414b39b8a8480627ff433d0f430dc08e7829d6
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-05-31 22:11:43 -0400

    Create 242 Valid Anagram.java

---
+++ 242 Valid Anagram/242 Valid Anagram.java
@@ -0,0 +1,27 @@
+//LeetCode Link: https://leetcode.com/problems/valid-anagram/
+//Youtube Solution: 
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+      
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] count = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
