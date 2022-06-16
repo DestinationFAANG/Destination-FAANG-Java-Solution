c1fedc4257f1d9840369f3c1ea9a65eb4321328d
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-06-16 11:02:03 -0400

    Create 5 Longest Palindromic Substring.java

---
+++ 5 Longest Palindromic Substring/5 Longest Palindromic Substring.java
@@ -0,0 +1,42 @@
+//Leetcode Link: https://leetcode.com/problems/longest-palindromic-substring/
+//Youtube Solution: 
+
+class Solution {
+    public String longestPalindrome(String s) {
+        
+        if(s == null || s.length() < 1){
+            return "";
+        }
+        int left = 0; int right = 0;
+        for(int i=0; i<s.length(); i++){
+            int len1 = checkPalindrome(s, i, i);
+            int len2 = checkPalindrome(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(len > right - left){
+                
+                left = i-(len - 1)/2;
+                right = i + len/2;
+            }
+        }
+        
+        return s.substring(left,right+1);
+        
+    }
+    
+    public int checkPalindrome(String s, int left, int right){
+        
+        int L = left, R = right;
+        
+        while(L >=0 && R<s.length() && s.charAt(L) == s.charAt(R)){
+            
+            L--;
+            R++;
+            
+            
+        }
+        
+        return R-L-1;
+        
+        
+    }
+}
