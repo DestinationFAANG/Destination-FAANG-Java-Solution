b0b967f91233aa863ccb840cb31863cbf7bd00ed
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-08-30 00:36:25 -0400

    Update 152 Maximum Product Subarray.java.java

--- 152 Maximum product SubArray/152 Maximum Product Subarray.java.java
+++ 152 Maximum product SubArray/152 Maximum Product Subarray.java.java
@@ -1,18 +1,31 @@
+// Best Leetcode problems for FAANG: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+// Video Solution: https://www.youtube.com/watch?v=OuRQ_TCCjsU
+// Leetcode Link: https://leetcode.com/problems/maximum-product-subarray/
+
+**************** Java Solution ***********************
+
+
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxProduct = 1;
-        int minProduct = 1;
-        int result = nums[0];
         
-        for(int i = 0; i<nums.length; i++){
+        if(nums.length == 0){
+            return 0;
+        }
+        
+        int min = nums[0];
+        int max = nums[0];
+        int result = max;
+        
+        for(int i=1; i<nums.length; i++){
+            int cur = nums[i];
+            int temp = Math.max(cur, Math.max(max*cur, min*cur));
+            min = Math.min(cur, Math.min(min*cur, max*cur));
+            max = temp;
             
-            int temp = maxProduct* nums[i];
-            maxProduct = Math.max(nums[i], Math.max(maxProduct* nums[i], minProduct* nums[i]));
-            minProduct = Math.min(nums[i], Math.min(temp, minProduct * nums[i]));
-            
-            result = Math.max(result, Math.max(minProduct, maxProduct));
+            result = Math.max(result, max);
         }
         
         return result;
+        
     }
 }
