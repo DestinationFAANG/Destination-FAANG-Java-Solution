16140aed863c4ce0421106f53b9d813f9a4c92b6
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-11-22 05:19:38 -0500

    Create 45 Jump Game - II.java

---
+++ 45 Jump Game - II/45 Jump Game - II.java
@@ -0,0 +1,25 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/jump-game-ii/
+
+// Video Solution: 
+
+**************** Java Solution ***********************
+
+
+class Solution {
+    public int jump(int[] nums) {
+        
+        int jump=0, currMax = 0, currEnd = 0;
+        
+        for(int i=0; i<nums.length-1; i++){
+            currMax = Math.max(currMax, i+nums[i]);
+            if(i == currEnd){
+                jump++;
+                currEnd = currMax;
+            }
+        }
+        
+        return jump;
+    }
+}
