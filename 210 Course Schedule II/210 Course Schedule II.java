327fbf0ec11a88280c9d30637c75e539848363b2
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-12-01 00:44:23 -0500

    Create 210 Course Schedule II.java

---
+++ 210 Course Schedule II/210 Course Schedule II.java
@@ -0,0 +1,89 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/race-car/
+
+// Video Solution: https://www.youtube.com/watch?v=_RWV4hZdmdk
+
+**************** Java Solution ***********************
+
+
+class Solution {
+  static int WHITE = 1;
+  static int GRAY = 2;
+  static int BLACK = 3;
+
+  boolean isPossible;
+  Map<Integer, Integer> color;
+  Map<Integer, List<Integer>> adjList;
+  List<Integer> topologicalOrder;
+
+  private void init(int numCourses) {
+    this.isPossible = true;
+    this.color = new HashMap<Integer, Integer>();
+    this.adjList = new HashMap<Integer, List<Integer>>();
+    this.topologicalOrder = new ArrayList<Integer>();
+
+    // By default all vertces are WHITE
+    for (int i = 0; i < numCourses; i++) {
+      this.color.put(i, WHITE);
+    }
+  }
+
+  private void dfs(int node) {
+
+    // Don't recurse further if we found a cycle already
+    if (!this.isPossible) {
+      return;
+    }
+
+    // Start the recursion
+    this.color.put(node, GRAY);
+
+    // Traverse on neighboring vertices
+    for (Integer neighbor : this.adjList.getOrDefault(node, new ArrayList<Integer>())) {
+      if (this.color.get(neighbor) == WHITE) {
+        this.dfs(neighbor);
+      } else if (this.color.get(neighbor) == GRAY) {
+        // An edge to a GRAY vertex represents a cycle
+        this.isPossible = false;
+      }
+    }
+
+    // Recursion ends. We mark it as black
+    this.color.put(node, BLACK);
+    this.topologicalOrder.add(node);
+  }
+
+  public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+    this.init(numCourses);
+
+    // Create the adjacency list representation of the graph
+    for (int i = 0; i < prerequisites.length; i++) {
+      int dest = prerequisites[i][0];
+      int src = prerequisites[i][1];
+      List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
+      lst.add(dest);
+      adjList.put(src, lst);
+    }
+
+    // If the node is unprocessed, then call dfs on it.
+    for (int i = 0; i < numCourses; i++) {
+      if (this.color.get(i) == WHITE) {
+        this.dfs(i);
+      }
+    }
+
+    int[] order;
+    if (this.isPossible) {
+      order = new int[numCourses];
+      for (int i = 0; i < numCourses; i++) {
+        order[i] = this.topologicalOrder.get(numCourses - i - 1);
+      }
+    } else {
+      order = new int[0];
+    }
+
+    return order;
+  }
+}
