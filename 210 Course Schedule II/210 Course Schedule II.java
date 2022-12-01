3a036328dbe2e07156362b39768e5723a499078e
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2022-12-01 01:34:45 -0500

    Update 210 Course Schedule II.java

--- 210 Course Schedule II/210 Course Schedule II.java
+++ 210 Course Schedule II/210 Course Schedule II.java
@@ -1,6 +1,6 @@
 // Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
 
-// Leetcode Link: https://leetcode.com/problems/race-car/
+// Leetcode Link: https://leetcode.com/problems/course-schedule-ii/
 
 // Video Solution: https://www.youtube.com/watch?v=_RWV4hZdmdk
 
