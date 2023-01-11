2b48c5ccef92a4de54e10c2178f8b511087f7d84
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2023-01-11 08:22:02 -0500

    Create Majority Element.java

---
+++ 169 Majority Element/Majority Element.java
@@ -0,0 +1,34 @@
+Most asked problems at FAANG companies- https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+Leetcode Link- https://leetcode.com/problems/majority-element/
+
+Video Solution -  https://www.youtube.com/watch?v=UoTI6zBIBMo
+
+
+class Solution {
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            }
+            else {
+                counts.put(num, counts.get(num)+1);
+            }
+        }
+        return counts;
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+}
