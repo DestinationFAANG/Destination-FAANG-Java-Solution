e9e008031f1902a5e7f30d45732ac690a1d71071
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2023-01-20 18:34:24 +0530

    Create Logger Rate Limiter.java

---
+++ 359 Logger Rate Limiter/Logger Rate Limiter.java
@@ -0,0 +1,35 @@
+Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+Leetcode Link: https://leetcode.com/problems/logger-rate-limiter/
+
+Video Solution: https://www.youtube.com/watch?v=gYNSX9yVuhQ
+
+**************** Java Solution ***********************
+
+
+class Logger {
+  private HashMap<String, Integer> msgDict;
+
+  /** Initialize your data structure here. */
+  public Logger() {
+    msgDict = new HashMap<String, Integer>();
+  }
+
+  /**
+   * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+   */
+  public boolean shouldPrintMessage(int timestamp, String message) {
+
+    if (!this.msgDict.containsKey(message)) {
+      this.msgDict.put(message, timestamp);
+      return true;
+    }
+
+    Integer oldTimestamp = this.msgDict.get(message);
+    if (timestamp - oldTimestamp >= 10) {
+      this.msgDict.put(message, timestamp);
+      return true;
+    } else
+      return false;
+  }
+}
