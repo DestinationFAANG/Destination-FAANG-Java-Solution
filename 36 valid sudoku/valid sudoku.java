1ab943ba4fe5b01205aa04699366bc99ab48687f
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2023-03-02 21:02:47 +0530

    Create valid sudoku.java

---
+++ 36 valid sudoku/valid sudoku.java
@@ -0,0 +1,55 @@
+Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+Leetcode Link: https://leetcode.com/problems/valid-sudoku/
+
+Video Solution: 
+
+**************** Java Solution ***********************
+
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+
+        // Use hash set to record the status
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        for (int r = 0; r < N; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                // Check if the position is filled with number
+                if (val == '.') {
+                    continue;
+                }
+
+                // Check the row
+                if (rows[r].contains(val)) {
+                    return false;
+                }
+                rows[r].add(val);
+
+                // Check the column
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
+            }
+        }
+        return true;
+    }
+}
