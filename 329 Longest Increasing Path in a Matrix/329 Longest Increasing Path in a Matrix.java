9ddc19471ff18b7f727b54362974665c61b9594b
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2023-10-17 20:25:46 -0400

    Create 329 Longest Increasing Path in a Matrix.java

---
+++ 329 Longest Increasing Path in a Matrix/329 Longest Increasing Path in a Matrix.java
@@ -0,0 +1,36 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+
+// Video Solution: https://www.youtube.com/watch?v=gvwTGXP-reQ
+
+**************** Java Solution ***********************
+
+
+// DFS + Memoization Solution
+// Accepted and Recommended
+public class Solution {
+    private static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int m, n;
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix.length == 0) return 0;
+        m = matrix.length; n = matrix[0].length;
+        int[][] cache = new int[m][n];
+        int ans = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                ans = Math.max(ans, dfs(matrix, i, j, cache));
+        return ans;
+    }
+
+    private int dfs(int[][] matrix, int i, int j, int[][] cache) {
+        if (cache[i][j] != 0) return cache[i][j];
+        for (int[] d : dirs) {
+            int x = i + d[0], y = j + d[1];
+            if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
+                cache[i][j] = Math.max(cache[i][j], dfs(matrix, x, y, cache));
+        }
+        return ++cache[i][j];
+    }
+}
