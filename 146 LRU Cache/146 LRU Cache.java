79277e0feae2d4c8f85b3e682b37b34d68459d99
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-01-15 23:34:11 -0500

    Create 146 LRU Cache.java

---
+++ 146 LRU Cache/146 LRU Cache.java
@@ -0,0 +1,80 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/lru-cache
+
+// Video Solution: https://www.youtube.com/watch?v=VPq5dlxaeP8
+
+**************** Java Solution ***********************
+
+
+import java.util.HashMap;
+
+public class LRUCache {
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final HashMap<Integer, Node> map;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        remove(node);
+        insertAtHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            insertAtHead(node);
+        } else {
+            if (map.size() == capacity) {
+                map.remove(tail.prev.key);
+                remove(tail.prev);
+            }
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            insertAtHead(newNode);
+        }
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertAtHead(Node node) {
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+}
+
+    
+    
