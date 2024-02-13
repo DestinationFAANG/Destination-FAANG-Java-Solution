f6232373908394dd71268c7209e8acaf14cc259c
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-12 23:19:18 -0500

    Create 155 Min Stack.java

---
+++ 155 Min Stack/155 Min Stack.java
@@ -0,0 +1,49 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/min-stack/
+
+// Video Solution: https://www.youtube.com/watch?v=PWoIZxcamsQ
+
+**************** Java Solution ***********************
+
+
+class MinStack {
+    private Node head;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
