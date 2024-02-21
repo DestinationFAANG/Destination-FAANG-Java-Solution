c2fc36ced11c2d9f2af8c566d693ffdb3a75939d
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-20 23:32:38 -0500

    Create 101 Symmetric Tree.java

---
+++ 101 Symmetric Tree/101 Symmetric Tree.java
@@ -0,0 +1,29 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/symmetric-tree/
+
+// Video Solution: https://www.youtube.com/watch?v=NeSeH2ECZUw
+
+**************** Java Solution ***********************
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return (left.val == right.val)
+            && isMirror(left.left, right.right)
+            && isMirror(left.right, right.left);
+    }
+}
