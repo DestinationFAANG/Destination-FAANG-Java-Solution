5d23d8e24c9f47f8954b242e1944525a2c9eeb35
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-20 23:36:21 -0500

    Create 150 Evaluate Reverse Polish Notation.java

---
+++ 150 Evaluate Reverse Polish Notation/150 Evaluate Reverse Polish Notation.java
@@ -0,0 +1,42 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+// Video Solution: https://www.youtube.com/watch?v=vDRZN5i4b8U
+
+**************** Java Solution ***********************
+
+import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop(); // Second operand
+                int a = stack.pop(); // First operand
+                int result = applyOperator(token, a, b);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperator(String operator, int a, int b) {
+        switch (operator) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+}
