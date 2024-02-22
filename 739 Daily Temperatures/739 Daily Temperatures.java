9aaf78c4065716bd44b87d460442b73ecfedf695
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-21 22:45:52 -0500

    Create 739 Daily Temperatures.java

---
+++ 739 Daily Temperatures/739 Daily Temperatures.java
@@ -0,0 +1,26 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.com/problems/daily-temperatures/
+
+// Video Solution: https://www.youtube.com/watch?v=jmuo4BBfl3I
+
+
+**************** Java Solution ***********************
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
