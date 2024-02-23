74246ee45c99f192410f4d91cd67a7564c6bc3c4
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-23 04:08:52 -0500

    Create 853 Car Fleet.java

---
+++ 853 Car Fleet/853 Car Fleet.java
@@ -0,0 +1,33 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.com/problems/car-fleet/
+
+// Video Solution: https://www.youtube.com/watch?v=jmuo4BBfl3I
+
+
+**************** Java Solution ***********************
+
+public class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] cars = new double[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+        
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+        
+        int count = 0;
+        double prevTime = 0;
+        for (double[] car : cars) {
+            if (car[1] > prevTime) {
+                count++;
+                prevTime = car[1];
+            }
+        }
+        
+        return count;
+    }
+}
