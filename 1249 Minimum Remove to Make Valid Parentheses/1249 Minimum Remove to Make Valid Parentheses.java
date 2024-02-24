1aaa49178941b19f6187af0af6f756710aa944df
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-23 21:44:51 -0500

    Create 1249 Minimum Remove to Make Valid Parentheses.java

---
+++ 1249 Minimum Remove to Make Valid Parentheses/1249 Minimum Remove to Make Valid Parentheses.java
@@ -0,0 +1,43 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+// Video Solution: https://www.youtube.com/watch?v=IasMlShanvc
+
+
+**************** Java Solution ***********************
+
+public class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> removeIndices = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    removeIndices.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        
+        // Add remaining unmatched opening parentheses indices to the set
+        while (!stack.isEmpty()) {
+            removeIndices.add(stack.pop());
+        }
+        
+        // Build the result string
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!removeIndices.contains(i)) {
+                result.append(s.charAt(i));
+            }
+        }
+        
+        return result.toString();
+    }
+}
