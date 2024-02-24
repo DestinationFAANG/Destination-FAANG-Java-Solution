fa20c2fe11bcf3d4e9b50bca3dcd2cd3a5869b80
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-24 16:27:15 -0500

    Create 704 Binary Search.java

---
+++ 704 Binary Search/704 Binary Search.java
@@ -0,0 +1,27 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.com/problems/binary-search/
+
+// Video Solution: https://www.youtube.com/watch?v=t3yPHFyKxr4
+
+
+**************** Java Solution ***********************
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1; // Target not found
+    }
+}
