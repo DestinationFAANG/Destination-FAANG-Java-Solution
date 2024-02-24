4bdc2eb2f5e4395e8e28958a46890c64f9a55b95
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-02-24 04:36:12 -0500

    Create 716 max stack.java

---
+++ 716 max stack/716 max stack.java
@@ -0,0 +1,51 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link:  https://leetcode.ca/all/716.html
+
+// Video Solution: https://www.youtube.com/watch?v=IasMlShanvc
+
+
+**************** Java Solution ***********************
+import java.util.Stack;
+
+class MaxStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> maxStack;
+
+    public MaxStack() {
+        stack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        int max = maxStack.isEmpty() ? x : Math.max(maxStack.peek(), x);
+        stack.push(x);
+        maxStack.push(max);
+    }
+
+    public int pop() {
+        maxStack.pop();
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int peekMax() {
+        return maxStack.peek();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack<>();
+        while (top() != max) {
+            buffer.push(pop());
+        }
+        pop(); // Remove the max element
+        while (!buffer.isEmpty()) {
+            push(buffer.pop());
+        }
+        return max;
+    }
+}
