c1e9318692ef02d6eeb81711741d2a55f99ebc74
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-01 01:16:18 -0500

    Create 875 Koko Eating Bananas.java

---
+++ 875 Koko Eating Bananas/875 Koko Eating Bananas.java
@@ -0,0 +1,34 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/koko-eating-bananas/
+
+// Video Solution: https://www.youtube.com/watch?v=dP4cb8zbohY
+
+
+**************** Java Solution ***********************
+class Solution {
+   public static int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canFinish(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private static boolean canFinish(int[] piles, int speed, int h) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += Math.ceil((double) pile / speed);
+        }
+        return hours <= h;
+    }
+}
