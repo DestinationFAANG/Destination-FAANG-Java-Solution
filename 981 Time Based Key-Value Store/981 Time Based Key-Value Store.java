160e3facf19825e96a62b5cca564810d19641789
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-03 16:51:19 -0500

    Create 981 Time Based Key-Value Store.java

---
+++ 981 Time Based Key-Value Store/981 Time Based Key-Value Store.java
@@ -0,0 +1,35 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/time-based-key-value-store/
+
+// Video Solution: https://www.youtube.com/watch?v=dP4cb8zbohY
+
+
+**************** Java Solution ***********************
+class TimeMap {
+private Map<String, TreeMap<Integer, String>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if (treeMap == null) {
+            return "";
+        }
+        Map.Entry<Integer, String> entry = treeMap.floorEntry(timestamp);
+        return entry == null ? "" : entry.getValue();
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
