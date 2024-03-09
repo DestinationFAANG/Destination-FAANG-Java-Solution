b17b93786c67e1bf12cc069b7a766fcc92873ac7
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-09 16:45:29 -0500

    Create 110 Balanced Binary Tree.java

---
+++ 110 Balanced Binary Tree/110 Balanced Binary Tree.java
@@ -0,0 +1,49 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/balanced-binary-tree/
+
+// Video Solution: https://www.youtube.com/watch?v=gu5rAEvm9Fk
+
+**************** Java Solution ***********************
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
