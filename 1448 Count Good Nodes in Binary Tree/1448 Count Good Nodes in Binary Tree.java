de6f16fc7488a88d00847da55eaab62aaaefad4a
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-09 17:25:37 -0500

    Create 1448 Count Good Nodes in Binary Tree.java

---
+++ 1448 Count Good Nodes in Binary Tree/1448 Count Good Nodes in Binary Tree.java
@@ -0,0 +1,47 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+// Video Solution: https://www.youtube.com/watch?v=4lMY-g0Afg8
+
+**************** Java Solution ***********************
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return countGoodNodes(root, Integer.MIN_VALUE);
+    }
+
+    private int countGoodNodes(TreeNode node, int maxSoFar) {
+        if (node == null) {
+            return 0;
+        }
+
+        int count = 0;
+        // Check if the current node is a good node
+        if (node.val >= maxSoFar) {
+            count = 1; // Current node is a good node
+            maxSoFar = node.val; // Update the maximum value along the path
+        }
+
+        // Count good nodes in the left and right subtrees
+        count += countGoodNodes(node.left, maxSoFar);
+        count += countGoodNodes(node.right, maxSoFar);
+
+        return count;
+    }
+}
