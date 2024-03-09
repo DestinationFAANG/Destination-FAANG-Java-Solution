a201f7b6c088d6f5d67ea0b8a22f97920df95467
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-09 17:20:06 -0500

    Create 199 Binary Tree Right Side View.java

---
+++ 199 Binary Tree Right Side View/199 Binary Tree Right Side View.java
@@ -0,0 +1,56 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/binary-tree-right-side-view/
+
+// Video Solution: https://www.youtube.com/watch?v=4lMY-g0Afg8
+
+**************** Java Solution ***********************
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // Add the last node's value of each level to the result list
+                if (i == levelSize - 1) {
+                    result.add(currentNode.val);
+                }
+
+                // Add child nodes to the queue for the next level
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
