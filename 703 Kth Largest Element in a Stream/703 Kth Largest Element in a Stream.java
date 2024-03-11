907f94acc4bf5321cdb699e338cab8458fbca4c5
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-11 02:24:14 -0400

    Create 703 Kth Largest Element in a Stream.java

---
+++ 703 Kth Largest Element in a Stream/703 Kth Largest Element in a Stream.java
@@ -0,0 +1,41 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+// Video Solution: https://www.youtube.com/watch?v=xq4RoS0vVfM
+
+**************** Java Solution ***********************
+
+
+class KthLargest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>(k); // Min-heap with a capacity of k
+
+        // Add initial elements to the heap
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.poll(); // Remove the smallest element
+            minHeap.offer(val); // Add the new value
+        }
+
+        return minHeap.peek(); // Return the kth largest element
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
