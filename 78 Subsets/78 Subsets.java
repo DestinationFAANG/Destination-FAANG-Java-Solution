0616f4080338362fad78e76c0a4913dc8f1973b3
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-27 00:51:53 -0400

    Create 78 Subsets.java

---
+++ 78 Subsets/78 Subsets.java
@@ -0,0 +1,25 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/subsets/
+
+// Video Solution: https://www.youtube.com/watch?v=OQKWzRJYtwg
+
+**************** Java Solution ***********************
+
+
+class Solution {
+     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(i + 1, nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
