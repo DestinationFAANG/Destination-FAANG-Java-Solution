8a5880c816abbc79c029d7359fbf144ef07c2c00
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-27 01:02:11 -0400

    Create 46 Permutations.java

---
+++ 46 permutations/46 Permutations.java
@@ -0,0 +1,39 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/permutations/
+
+// Video Solution: https://www.youtube.com/watch?v=_tgwDDWuU-Q
+
+**************** Java Solution ***********************
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(result, new ArrayList<>(), nums, used);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                current.add(nums[i]);
+                used[i] = true;
+                backtrack(result, current, nums, used);
+                used[i] = false;
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    
+}
