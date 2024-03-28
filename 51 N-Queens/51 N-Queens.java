227b72ba983e05bab32fb0ca30de04369ef23ac8
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-28 01:10:49 -0400

    Create 51 N-Queens.java

---
+++ 51 N-Queens/51 N-Queens.java
@@ -0,0 +1,67 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/n-queens/
+
+// Video Solution: https://www.youtube.com/watch?v=GPigeECXWZE
+
+**************** Java Solution ***********************
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        backtrack(board, 0, result);
+        return result;
+    }
+
+    private void backtrack(char[][] board, int col, List<List<String>> result) {
+        if (col == board.length) {
+            result.add(construct(board));
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (isValid(board, i, col)) {
+                board[i][col] = 'Q';
+                backtrack(board, col + 1, result);
+                board[i][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String row = new String(board[i]);
+            result.add(row);
+        }
+        return result;
+    }
+}
