36253c45d24ccdc4ba899801b8eafdb0e6d435dd
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-03-27 23:24:28 -0500

    Create 131 Palindrome Partitioning.java

---
+++ 131 Palindrome Partitioning/131 Palindrome Partitioning.java
@@ -0,0 +1,39 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/palindrome-partitioning/
+
+// Video Solution: https://www.youtube.com/watch?v=GPigeECXWZE
+
+**************** Java Solution ***********************
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> current, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(result, current, s, end + 1);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
