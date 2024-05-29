3127d666039cd94938a6868188c734793ae21014
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-05-29 00:20:02 -0400

    Create 1143 Longest Common Subsequence.java

---
+++ 1143 Longest Common Subsequence/1143 Longest Common Subsequence.java
@@ -0,0 +1,28 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/longest-common-subsequence/
+
+// Video Solution: https://www.youtube.com/watch?v=A3LYMRqAQeU
+
+**************** Java Solution ***********************
+
+class Solution {
+        public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
