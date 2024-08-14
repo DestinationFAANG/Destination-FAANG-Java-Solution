5ba23ef3a093bb85c4d74ed72b3208257c6e30e2
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-08-14 00:03:14 -0400

    Create 202 Happy Number.java

---
+++ 202 Happy Number/202 Happy Number.java
@@ -0,0 +1,29 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/happy-number/
+
+// Video Solution: https://www.youtube.com/watch?v=d5SvCvRmmww
+
+**************** Java Solution ***********************
+class Solution {
+    public static boolean isHappy(int n) {
+        Set<Integer> seenNumbers = new HashSet<>();
+
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = getSumOfSquares(n);
+        }
+
+        return n == 1;
+    }
+
+    private static int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
