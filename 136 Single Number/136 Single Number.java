bc8e2b77537256aab05a7d5cc8ba1529f6f73ce0
Destination FAANG <104370349+DestinationFAANG@users.noreply.github.com>
2024-08-31 16:08:51 -0400

    Create 136 Single Number.java

---
+++ 136 Single Number/136 Single Number.java
@@ -0,0 +1,17 @@
+// Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
+
+// Leetcode Link: https://leetcode.com/problems/single-number/
+
+// Video Solution: https://www.youtube.com/watch?v=2LaOz13Y5qw
+
+**************** Java Solution ***********************
+
+class Solution {
+     public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR operation
+        }
+        return result;
+    }
+}
